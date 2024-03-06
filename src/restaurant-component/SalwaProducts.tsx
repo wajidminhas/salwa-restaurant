64d9a65ff1f33c781539ef9758851c7e05ec14c6
wajidminhas <shanitent667@gmail.com>
2024-03-06 00:49:26 -0800

    salwaproducts edited

--- src/restaurant-component/SalwaProducts.tsx
+++ src/restaurant-component/SalwaProducts.tsx
@@ -1,40 +1,38 @@
-
-import React from 'react'
-import {productList} from "@/restaurant-component/ProductsList";
-
-
+import { productList } from "@/restaurant-component/productsList";
+import Image from "next/image";
+import React from "react";
 
 const SalwaProducts = () => {
-  return (
-      <div className=" w-screen overflow-x-scroll">
-        <div className='w-max flex'>
-        {productList.map((product) => (
-      <div key={product.id} 
-       className=" w-full mt-4 md:w-[300px] rounded-md border mx-10">
-        
-          <img
-              src={product.img}
-              alt="Laptop"
-              className=" object-contain"
-          />
-          <div className="p-4">
-              <h1 className="text-lg font-semibold">{product.title}</h1>
-              <p className="mt-3 text-sm text-gray-600">
-                 {product.desc}
-              </p>
-                    <h1 className="text-sm pt-3 font-semibold">{product.title}</h1>
-              <button
-                  type="button"
-                  className="mt-4 rounded-sm bg-black px-2.5 py-1 text-[10px] font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-              >
-                  Read
-              </button>
-          </div>
-          </div>
+    return (
+        <div className="w-screen overflow-x-scroll text-red-500">
+            {/* WRAPPER */}
+            <div className="w-max flex">
+                {/* SINGLE ITEM */}
+                {productList.map((item) => (
+                    <div
+                        key={item.id}
+                        className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]"
+                    >
+                        {/* IMAGE CONTAINER */}
+                        {item.img && (
+                            <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500">
+                                <Image src={item.img} alt="" fill className="object-contain" />
+                            </div>
+                        )}
+                        {/* TEXT CONTAINER */}
+                        <div className=" flex-1 flex flex-col items-center justify-center text-center gap-4">
+                            <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">{item.title}</h1>
+                            <p className="p-4 2xl:p-8">{item.desc}</p>
+                            <span className="text-xl font-bold">${item.price}</span>
+                            <button className="bg-red-500 text-white p-2 rounded-md">
+                                Add to Cart
+                            </button>
+                        </div>
+                    </div>
                 ))}
-          </div>
-      </div>
-  )
-}
+            </div>
+        </div>
+    );
+};
 
-export default SalwaProducts
+export default SalwaProducts;
