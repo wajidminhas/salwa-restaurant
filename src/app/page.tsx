35606665b2e5408ad860394dc60f6f896c455407
wajidminhas <shanitent667@gmail.com>
2024-03-06 02:05:12 -0800

    Hot Offer component is added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 
+import CountDown from "@/restaurant-component/Countdown";
 import { HeroOne } from "@/restaurant-component/HeroOne";
 import HeroTwo from "@/restaurant-component/HeroTwo";
+import HotOffer from "@/restaurant-component/HotOffer";
 import SalwaProducts from "@/restaurant-component/SalwaProducts";
 import Image from "next/image";
 
@@ -10,6 +12,7 @@ export default function Home() {
      {/* <HeroOne /> */}
      <HeroTwo />
      <SalwaProducts />
+     <HotOffer />
     </div>
   );
 }
--- src/restaurant-component/SalwaProducts.tsx
+++ src/restaurant-component/SalwaProducts.tsx
@@ -6,25 +6,25 @@ const SalwaProducts = () => {
     return (
         <div className="w-screen overflow-x-scroll text-red-500">
             {/* WRAPPER */}
-            <div className="w-max flex">
+            <div className="w-max  flex">
                 {/* SINGLE ITEM */}
                 {productList.map((item) => (
                     <div
                         key={item.id}
-                        className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]"
+                        className="w-screen h-[60vh] flex flex-col border border-gray-500 rounded-md  items-center justify-around p-4 mx-2 hover:bg-fuchsia-50 transition-all duration-300 md:w-[30vw] xl:w-[20vw] xl:h-[70vh]"
                     >
                         {/* IMAGE CONTAINER */}
                         {item.img && (
-                            <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500">
-                                <Image src={item.img} alt="" fill className="object-contain" />
+                            <div className="relative flex-1 w-[90%]  hover:rotate-[60deg] transition-all duration-500">
+                                <Image src={item.img} alt="" fill className="object-contain items-center" />
                             </div>
                         )}
                         {/* TEXT CONTAINER */}
-                        <div className=" flex-1 flex flex-col items-center justify-center text-center gap-4">
-                            <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">{item.title}</h1>
-                            <p className="p-4 2xl:p-8">{item.desc}</p>
-                            <span className="text-xl font-bold">${item.price}</span>
-                            <button className="bg-red-500 text-white p-2 rounded-md">
+                        <div className=" flex-1 flex flex-col items-center justify-center text-center gap-1">
+                            <h1 className="text-xl font-bold uppercase xl:text-xl ">{item.title}</h1>
+                            <p className="xl text-black line-clamp-2">{item.desc}</p>
+                            <span className=" py-2text-xl  font-bold">${item.price}</span>
+                            <button className="bg-red-700 text-white p-2 rounded-md hover:bg-white hover:text-red-700 hover:font-bold hover:border  hover:border-gray-500">
                                 Add to Cart
                             </button>
                         </div>
